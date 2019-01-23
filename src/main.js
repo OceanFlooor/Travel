cf0d86fa6e6591506121116247680492aaaacfb6
Raven <836173483@qq.com>
2019-01-23 22:12:57 +0800

    project-init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import '@/assets/style/reset.css'
+import '@/assets/style/border.css'
 
 Vue.config.productionTip = false
+
+// 解决移动端点击事件300毫秒延迟问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
